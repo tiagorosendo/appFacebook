f06c3b1d9ea1257bcb6f09986819ee91eef716cc
Tiago <tiago@scesistemas.com.br>
2016-01-29 16:26:35 -0200

    Alter facebookService

--- FacebookService.js
+++ FacebookService.js
@@ -9,12 +9,21 @@
 
     function FacebookService($timeout) {
         var service = this;
-        service.methods = sendGroupImage;
+        service.methods = {};
+        service.methods.sendGroupImage = sendGroupImage;
+        service.methods.getGroupList = getGroupList;
 
         function sendGroupImage(param) {
             return FB.api('/' + param.id + '/photos', 'post', param.dados, function(response) {
                 return response;
             });
+        };
+
+        function getGroupList() {
+            return FB.api('/me/groups', function(response) {
+                return response;
+            });
         }
     }
+
 })();
