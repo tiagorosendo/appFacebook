92668fca3807e116d3f74cd78ff38b71c1cc3b5a
Tiago Rosendo da Silva <Tiago Rosendo da Silva>
2016-02-05 00:18:13 -0200

    methods

--- app/SpamController.js
+++ app/SpamController.js
@@ -49,7 +49,7 @@
             for (var x = 0, ln = vm.listaSelecionados.length; x < ln; x++) {
                 setTimeout(function(y) {
                     params.id = vm.listaSelecionados[y].id;
-                    FacebookService.sendGroupImage(params).then(function(response) {
+                    FacebookService.methods.sendGroupImage(params).then(function(response) {
                         vm.listaSelecionados[y] = (!response || response.error);
                     });
 
